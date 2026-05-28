b1115f5cd5ddf7c7c3d6e9c33203b5f3e9db1caf
lucxpo <187343698+lucxpo@users.noreply.github.com>
2026-05-28 09:52:49 -0300

    add text_node_to_html_node function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    t = TextNode("bacon is good", TextType.PLAIN)
+    t = TextNode("bacon is good", TextType.TEXT)
     print(t)
 
 
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -12,11 +13,22 @@ class TestTextNode(unittest.TestCase):
     def test_not_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text ode", TextType.BOLD)
-        node3 = TextNode("This is a text node", TextType.PLAIN)
+        node3 = TextNode("This is a text node", TextType.TEXT)
         node4 = TextNode("This is a text node", TextType.BOLD, url="www.bacon.com")
         self.assertNotEqual(node, node2)
         self.assertNotEqual(node, node3)
         self.assertNotEqual(node, node4)
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+        node2 = TextNode("bacon", TextType.BOLD)
+        html_node2 = text_node_to_html_node(node2)
+        self.assertEqual(html_node2.tag, "b")
+        self.assertEqual(html_node2.value, "bacon")
 
 if __name__ == "__main__":
     unittest.main()
